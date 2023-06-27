d2e20a1e8ce1d1c0c87a29d7f7748b275f89412b
Federico Nery <fedegnery@gmail.com>
2023-06-26 22:40:40 -0300

    Initial Commit

---
+++ android/app/src/main/kotlin/dev/federiconery/digimon_world_adventure_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.federiconery.digimon_world_adventure_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/distance_tracker.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+
+class DistanceTracker extends StatefulWidget {
+  @override
+  _DistanceTrackerState createState() => _DistanceTrackerState();
+}
+
+class _DistanceTrackerState extends State<DistanceTracker> {
+  double _totalDistance = 0.0;
+  double _previousValue = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    accelerometerEvents.listen(_onAccelerometerEvent);
+  }
+
+  void _onAccelerometerEvent(AccelerometerEvent event) {
+    double currentValue = event.x + event.y + event.z;
+    double delta = (currentValue - _previousValue).abs();
+    setState(() {
+      _totalDistance += delta;
+      _previousValue = currentValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Distance Tracker'),
+      ),
+      body: Center(
+        child: Text(
+          'Total Distance: ${_totalDistance.toStringAsFixed(2)}',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Distance Tracker Example',
+      home: DistanceTracker(),
+    );
+  }
+}
+
+*/
