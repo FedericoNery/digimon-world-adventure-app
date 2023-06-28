0a5e88e777d053a9a54a3767a1787400a16fc9d8
Federico Nery <fedegnery@gmail.com>
2023-06-28 15:41:28 -0300

    DWA-APP#2 :: Agregar lógica para que al avanzar de distancia el Digimon evolucione y vaya mostrando sus evoluciones

--- lib/distance_tracker.dart
+++ lib/distance_tracker.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
+import 'training.dart';
 
 class DistanceTracker extends StatefulWidget {
   @override
@@ -9,33 +10,59 @@ class DistanceTracker extends StatefulWidget {
 class _DistanceTrackerState extends State<DistanceTracker> {
   double _totalDistance = 0.0;
   double _previousValue = 0.0;
+  bool _isMounted = false;
 
   @override
   void initState() {
     super.initState();
+    _isMounted = true;
     accelerometerEvents.listen(_onAccelerometerEvent);
   }
 
   void _onAccelerometerEvent(AccelerometerEvent event) {
-    double currentValue = event.x + event.y + event.z;
-    double delta = (currentValue - _previousValue).abs();
-    setState(() {
-      _totalDistance += delta;
-      _previousValue = currentValue;
-    });
+    if (_isMounted) {
+      double currentValue = event.x + event.y + event.z;
+      double delta = (currentValue - _previousValue).abs();
+      setState(() {
+        _totalDistance += delta;
+        _previousValue = currentValue;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _isMounted = false;
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+
+    void _redirectToTraining() {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => Training(),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Distance Tracker'),
       ),
-      body: Center(
-        child: Text(
-          'Total Distance: ${_totalDistance.toStringAsFixed(2)}',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: Column(
+        children: [
+          Text(
+            'Total Distance: ${_totalDistance.toStringAsFixed(2)}',
+            style: TextStyle(fontSize: 24),
+          ),
+        ]
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _redirectToTraining,
+        tooltip: 'Training Area',
+        child: const Icon(Icons.fitness_center),
       ),
     );
   }
